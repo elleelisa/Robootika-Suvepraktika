f57d8b80b6171fcb17bac0f41f3e6d10d1d080b5
elleelisa <elleelisa@users.noreply.github.com>
2017-06-21 00:43:28 +0300

    last posts of kiirkursus

--- kiirkursus.js
+++ kiirkursus.js
@@ -4,16 +4,25 @@ function switchPage(page) {
     document.getElementById("firstPage").style.display = "block";
     document.getElementById("secondPage").style.display = "none";
     document.getElementById("thirdPage").style.display = "none";
+    document.getElementById("fourthPage").style.display = "none";
     break;
     case 2:
     document.getElementById("firstPage").style.display = "none";
     document.getElementById("secondPage").style.display = "block";
     document.getElementById("thirdPage").style.display = "none";
+    document.getElementById("fourthPage").style.display = "none";
     break;
     case 3:
     document.getElementById("firstPage").style.display = "none";
     document.getElementById("secondPage").style.display = "none";
     document.getElementById("thirdPage").style.display = "block";
+    document.getElementById("fourthPage").style.display = "none";
+    break;
+    case 4:
+    document.getElementById("firstPage").style.display = "none";
+    document.getElementById("secondPage").style.display = "none";
+    document.getElementById("thirdPage").style.display = "none";
+    document.getElementById("fourthPage").style.display = "block";
     break;
   }
   coloredButton(page);
@@ -27,16 +36,25 @@ function coloredButton(page){
     document.getElementById("firstPageButton").classList.add("w3-black");
     document.getElementById("secondPageButton").classList.remove("w3-black");
     document.getElementById("thirdPageButton").classList.remove("w3-black");
+    document.getElementById("fourthPageButton").classList.remove("w3-black");
     break;
     case 2:
     document.getElementById("secondPageButton").classList.add("w3-black");
     document.getElementById("firstPageButton").classList.remove("w3-black");
     document.getElementById("thirdPageButton").classList.remove("w3-black");
+    document.getElementById("fourthPageButton").classList.remove("w3-black");
     break;
     case 3:
     document.getElementById("thirdPageButton").classList.add("w3-black");
     document.getElementById("firstPageButton").classList.remove("w3-black");
     document.getElementById("secondPageButton").classList.remove("w3-black");
+    document.getElementById("fourthPageButton").classList.remove("w3-black");
+    break;
+    case 4:
+    document.getElementById("fourthPageButton").classList.add("w3-black");
+    document.getElementById("firstPageButton").classList.remove("w3-black");
+    document.getElementById("secondPageButton").classList.remove("w3-black");
+    document.getElementById("thirdPageButton").classList.remove("w3-black");
     break;
 
   }
